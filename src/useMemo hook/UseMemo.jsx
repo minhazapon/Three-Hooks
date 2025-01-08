ed8ba9eacb201d49c111b5d056e5d427aba32cda
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-01-07 18:48:38 -0800

    add useReducer Hook

--- src/useMemo hook/UseMemo.jsx
+++ src/useMemo hook/UseMemo.jsx
@@ -3,7 +3,7 @@
 const UseMemo = () => {
     return (
         <div>
-             cxwwdqefwefewfwefwf 
+       
         </div>
     );
 };
--- src/useReducer Hook/UseReducer.jsx
+++ src/useReducer Hook/UseReducer.jsx
@@ -1,11 +1,30 @@
+import { useReducer } from "react";
 
 
 const UseReducer = () => {
 
+    const reducer = (state, action) =>{
+        if(action.type === "INCREMENT"){
+            return state + 1
+        } 
+        if(action.type === "DECREMENT"){
+            return state - 1
+        } 
+    }
+
+    const [count, dispatch] = useReducer(reducer, 0)
     
     return (
-        <div>
-            
+        <div className=" mt-16 mb-16 ml-16 mr-16 ">
+            <div className=" flex justify-center items-center gap-5 ">
+
+                <button onClick={() => dispatch({type:'INCREMENT'})} className=" btn bg-cyan-500 text-white ">
+                Increment</button>
+                <p className=" text-xl text-black "> {count} </p>
+                <button onClick={() => dispatch({type:'DECREMENT'})} className=" btn bg-cyan-500 text-white ">
+                Decrement</button>
+
+            </div>
         </div>
     );
 };
